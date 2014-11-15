383db4ab1aabe5d11d91fd75220fbe0711be43d0
Pedro J. Parra <pedroparra.correo@gmail.com>
2014-11-15 23:45:16 +0100

    Fix test

--- test/test.js
+++ test/test.js
@@ -3,27 +3,22 @@ var assert = require('assert');
 var jshint = require('jshint/src/cli').run;
 var reporter = require('../reporter').reporter;
 
-describe('Cool reporter', function(){
+it('should be used by JSHint', function(){
 
-  it('should be used by JSHint', function(){
+  var ret = false;
+  var _log = process.stdout.write.bind(process.stdout);
 
-    var ret = false;
-    var _log = process.stdout.write.bind(process.stdout);
+  process.stdout.write = function (str) {
+    _log(str);
+    if ( /'foo' is defined/ig.test(str || '') ) { ret = true; }
+  };
 
-    process.stdout.write = function (str) {
-      _log(str);
-      if ( /'foo' is defined/ig.test(str || '') ) { ret = true; }
-    };
-
-    jshint({
-      args: ['fixture.js'],
-      reporter: reporter
-    });
-
-    process.stdout.write = _log;
-
-    assert(ret);
-
+  jshint({
+    args: ['fixture.js'],
+    reporter: reporter
   });
+
+  process.stdout.write = _log;
+  assert(ret);
 
 });
