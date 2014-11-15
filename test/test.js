0f7190090d154fb6265b3ac458954be6000b9575
Pedro J. Parra <pedroparra.correo@gmail.com>
2014-11-15 23:01:44 +0100

    Init tests

---
+++ test/test.js
@@ -0,0 +1,39 @@
+describe('Cool reporter', function(){
+
+  describe('No errors', function(){
+
+    describe('value_undefined', function(){
+      it('Return No problems', function(done){
+        done();
+      });
+    });
+
+    describe('undefined_value', function(){
+      it('Return No problems', function(done){
+        done();
+      });
+    });
+
+    describe('empty_value', function(){
+      it('Return No problems', function(done){
+        done();
+      });
+    });
+
+    describe('empty_array', function(){
+      it('Return No problems', function(done){
+        done();
+      });
+    });
+
+    describe('empty_object', function(){
+      it('Return No problems', function(done){
+        done();
+      });
+    });
+
+  });
+
+  // describe('Errors', function(){});
+
+});
